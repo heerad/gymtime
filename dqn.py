ddc11ae7195b7268e77132dec4c8e441e548128a
heerad <hfarkhoor24@gmail.com>
2017-07-19 16:13:52 -0700

    change params for LunarLander-v2

--- dqn.py
+++ dqn.py
@@ -22,25 +22,26 @@ from collections import deque
 #####################################################################################################
 ## Setup
 
-env_to_use = 'CartPole-v0'
+env_to_use = 'LunarLander-v2'
 
 # hyperparameters
-gamma = 1.				# reward discount factor
-h = 8					# hidden layer size
-lr = 1e-3				# learning rate
-lr_decay = 0.999		# learning rate decay (per episode)
-l2_reg = 0				# L2 regularization factor
+gamma = 1				# reward discount factor
+h = 256					# hidden layer size
+lr = 1e-4				# learning rate
+lr_decay = 1			# learning rate decay (per episode)
+l2_reg = 1e-7				# L2 regularization factor
 dropout = 0				# dropout rate (0 = no dropout)
-num_episodes = 500	# number of episodes
+num_episodes = 5000		# number of episodes
 max_steps_ep = 10000	# default max number of steps per episode (unless env has a lower hardcoded limit)
-slow_target_burnin = 1e3		# number of steps where slow target weights are tied to current network weights
+slow_target_burnin = 1e4		# number of steps where slow target weights are tied to current network weights
 update_slow_target_every = 1e2	# number of steps to use slow target as target before updating it to latest weights
 train_every = 1			# number of steps to run the policy (and collect experience) before updating network weights
-replay_memory_capacity = 2**20	# capacity of experience replay memory
-minibatch_size = 2**8	# size of minibatch from experience replay memory for updates
+replay_memory_capacity = int(1e5)	# capacity of experience replay memory
+minibatch_size = 1024	# size of minibatch from experience replay memory for updates
 epsilon_start = 1.0		# probability of random action at start
-epsilon_end = 0.05		# minimum probability of random action
-epsilon_decay_length = 1e3		# number of steps over which to linearly decay epsilon
+epsilon_end = 0.05		# minimum probability of random action after linear decay period
+epsilon_decay_length = 1e5		# number of steps over which to linearly decay epsilon
+epsilon_decay_exp = 0.97	# exponential decay rate after reaching epsilon_end (per episode)
 
 # game parameters
 env = gym.make(env_to_use)
@@ -74,7 +75,8 @@ info['params'] = dict(
 	minibatch_size = minibatch_size,
 	epsilon_start = epsilon_start,
 	epsilon_end = epsilon_end,
-	epsilon_decay_length = epsilon_decay_length
+	epsilon_decay_length = epsilon_decay_length,
+	epsilon_decay_exp = epsilon_decay_exp
 )
 
 #####################################################################################################
@@ -100,7 +102,9 @@ def generate_network(s, trainable, reuse):
 	hidden_drop = tf.layers.dropout(hidden, rate = dropout, training = trainable & is_training_ph)
 	hidden_2 = tf.layers.dense(hidden_drop, h, activation = tf.nn.relu, trainable = trainable, name = 'dense_1', reuse = reuse)
 	hidden_drop_2 = tf.layers.dropout(hidden_2, rate = dropout, training = trainable & is_training_ph)
-	action_values = tf.squeeze(tf.layers.dense(hidden_drop_2, n_actions, trainable = trainable, name = 'dense_2', reuse = reuse))
+	hidden_3 = tf.layers.dense(hidden_drop_2, h, activation = tf.nn.relu, trainable = trainable, name = 'dense_2', reuse = reuse)
+	hidden_drop_3 = tf.layers.dropout(hidden_3, rate = dropout, training = trainable & is_training_ph)
+	action_values = tf.squeeze(tf.layers.dense(hidden_drop_3, n_actions, trainable = trainable, name = 'dense_3', reuse = reuse))
 	return action_values
 
 with tf.variable_scope('q_network') as scope:
@@ -129,10 +133,10 @@ update_slow_target_op = tf.group(*update_slow_target_ops, name='update_slow_targ
 # Q-learning targets y_i for (s,a) from experience replay
 # = r_i + gamma*Q_slow(s',argmax_{a}Q(s',a)) if s' is not terminal
 # = r_i if s' terminal
-# targets = reward_ph + is_not_terminal_ph * gamma * tf.reduce_max(slow_target_action_values, axis=1)
 # Note that we're using Q_slow(s',argmax_{a}Q(s',a)) instead of max_{a}Q_slow(s',a) to address the maximization bias problem via Double Q-Learning
 targets = reward_ph + is_not_terminal_ph * gamma * \
 	tf.gather_nd(slow_target_action_values, tf.stack((tf.range(minibatch_size), tf.cast(tf.argmax(q_action_values_next, axis=1), tf.int32)), axis=1))
+# targets = reward_ph + is_not_terminal_ph * gamma * tf.reduce_max(slow_target_action_values, axis=1)
 
 # Estimated Q values for (s,a) from experience replay
 estim_taken_action_vales = tf.gather_nd(q_action_values, tf.stack((tf.range(minibatch_size), action_ph), axis=1))
@@ -156,7 +160,8 @@ sess.run(tf.global_variables_initializer())
 total_steps = 0
 experience = deque(maxlen=replay_memory_capacity)
 
-epsilon_step = (epsilon_start-epsilon_end)/epsilon_decay_length
+epsilon = epsilon_start
+epsilon_linear_step = (epsilon_start-epsilon_end)/epsilon_decay_length
 
 for ep in range(num_episodes):
 
@@ -168,12 +173,6 @@ for ep in range(num_episodes):
 	# env.render()
 
 	for t in range(max_steps_ep):
-
-		# linearly decay epsilon from epsilon_start to epsilon_end over epsilon_decay_length steps
-		epsilon = epsilon_start-epsilon_step*min(total_steps,epsilon_decay_length)
-
-		if total_steps == epsilon_decay_length:
-			print('--------------------------------STOPPED DECAYING EPSILON---------------------------------------------')
 
 		# choose action according to epsilon-greedy policy wrt Q
 		if np.random.random() < epsilon:
@@ -216,13 +215,23 @@ for ep in range(num_episodes):
 		observation = next_observation
 		total_steps += 1
 		steps_in_ep += 1
+
+		# linearly decay epsilon from epsilon_start to epsilon_end over epsilon_decay_length steps
+		if total_steps < epsilon_decay_length:
+			epsilon -= epsilon_linear_step
+		# then exponentially decay it every episode
+		elif done:
+			epsilon *= epsilon_decay_exp
+
+		if total_steps == epsilon_decay_length:
+			print('--------------------------------MOVING TO EXPONENTIAL EPSILON DECAY-----------------------------------------')
 		
 		if done: 
 			# Increment episode counter
 			_ = sess.run(episode_inc_op)
 			break
 
-	print('Episode %2i, Reward: %7.3f, Steps: %i'%(ep,total_reward,steps_in_ep))
+	print('Episode %2i, Reward: %7.3f, Steps: %i, Next eps: %7.3f'%(ep,total_reward,steps_in_ep, epsilon))
 
 # Finalize and upload results
 writefile('info.json', json.dumps(info))
